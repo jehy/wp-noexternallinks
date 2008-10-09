9d24de32ad24727af11d9d904d2ff5595ea7fef9
Jehy <Jehy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-10-09 13:43:59 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-noexternallinks/trunk@68263 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- goto.php
+++ goto.php
@@ -1,5 +1,6 @@
 <?php
-include 'lang.eng.inc';
+include 'lang.rus.inc';
+header('Content-type: text/html; charset="utf-8"',true);
 $url=$QUERY_STRING;
 if($url)$url='http://'.$url;
 if($url) @header('Location: '.$url);
@@ -7,10 +8,13 @@ if($url) @header('Location: '.$url);
 <html>
 	<head>
 		<title><?php echo WPNEL_REDIRECTING;?></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta name="robots" content="noindex,nofollow" />
 		<?php if($url) echo '<meta http-equiv="refresh" content="0; url='.$url.';" />'; ?>
 	</head>
 	<body style="margin:0;">
-		<?php echo WPNEL_REDIRECT1;?> <a href="<?php echo php echo $url?>"><?php echo =WPNEL_REDIRECT2;?></a><?php echo WPNEL_REDIRECT3. $url.WPNEL_REDIRECT4;?> 
+	<div align="center" style="margin-top: 15em;">
+		<?php echo WPNEL_REDIRECT1;?> <a href="<?php echo $url?>"><?php echo WPNEL_REDIRECT2;?></a><?php echo WPNEL_REDIRECT3.$url.WPNEL_REDIRECT4;?> 
+	</div>
 	</body>
 </html>
